a484e80ebc854fcfd5c13e3b78adb999e844bf02
Mandy <mandy@mandys-mbp.attlocal.net>
2021-02-03 00:21:36 -0600

    reward program

---
+++ src/api/dataService.js
@@ -0,0 +1,52 @@
+
+
+export default function() {
+  return Promise.resolve(
+    [
+        {
+          custid: 1,
+          name: "A",
+          amt: 120,
+          transactionDt: "05-01-2021"
+        },
+        {
+          custid: 1,
+          name: "A",
+          amt: 75,
+          transactionDt: "05-21-2021"
+        },
+        {
+          custid: 1,
+          name: "A",
+          amt: 94,
+          transactionDt: "05-21-2021"
+        },
+        {
+          custid: 1,
+          name: "A",
+          amt: 10,
+          transactionDt: "06-01-2021"
+        },
+        {
+          custid: 1,
+          name: "A",
+          amt: 75,
+          transactionDt: "06-21-2021"
+        },
+        {
+          custid: 1,
+          name: "A",
+          amt: 200,
+          transactionDt: "07-01-2021"
+        },
+        {
+          custid: 1,
+          name: "Acme",
+          amt: 1,
+          transactionDt: "07-04-2021"
+        },
+      
+      
+    ]
+  );
+};
